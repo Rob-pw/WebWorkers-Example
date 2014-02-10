52824906e923c44d96895e8c9a57310a63e7aa5b
Robert White <me@rob.pw>
2014-02-10 10:29:17 +0000

    Removed self-executing function

--- js/Countdown.js
+++ js/Countdown.js
@@ -3,56 +3,49 @@
  */
 
 /*
-    I like using self-executing functions,
-    they make it particularly easy to avoid
-    polluting the global namespace.
+    Let's create a new Worker,
+    we need to provide it the location (from the HTML page)
+    of the script to execute.
  */
-(function() {
-    /*
-        Let's create a new Worker,
-        we need to provide it the location (from the HTML page)
-        of the script to execute.
-     */
-    var dediWorker = new Worker('js/WebWorker::Countdown.js')
-        , output = document.getElementsByTagName('pre')[0]
-        , messageCount = 0;
+var dediWorker = new Worker('js/WebWorker::Countdown.js')
+    , output = document.getElementsByTagName('pre')[0]
+    , messageCount = 0;
+
+/*
+    Every time a message is received from the Worker,
+    execute this function.
+ */
+dediWorker.onmessage = function(e) {
+    e = e.data;
+    messageCount += 1;
 
     /*
-        Every time a message is received from the Worker,
-        execute this function.
+        After 5 messages
      */
-    dediWorker.onmessage = function(e) {
-        e = e.data;
-        messageCount += 1;
+    if(messageCount === 5) {
+        /*
+            Send a message to the worker,
+            telling it to pause.
+         */
+        dediWorker.postMessage({
+            command : "pause"
+        });
 
         /*
-            After 5 messages
+            Then after 3.5s, let's resume work.
          */
-        if(messageCount === 5) {
-            /*
-                Send a message to the worker,
-                telling it to pause.
-             */
+        setTimeout(function() {
             dediWorker.postMessage({
-                command : "pause"
+               command : "resume"
             });
+        }, 2500);
+    }
+    output.innerText += "(" + e.status + ") : " + e.current + "\n";
+};
 
-            /*
-                Then after 3.5s, let's resume work.
-             */
-            setTimeout(function() {
-                dediWorker.postMessage({
-                   command : "resume"
-                });
-            }, 2500);
-        }
-        output.innerText += "(" + e.status + ") : " + e.current + "\n";
-    };
-
-    /*
-        Worker does nothing until the first message
-     */
-    dediWorker.postMessage({
-        command : "start"
-    });
-})();
+/*
+    Worker does nothing until the first message
+ */
+dediWorker.postMessage({
+    command : "start"
+});
